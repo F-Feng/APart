5f23ef43aea25e5ffcb83b24dd3700721c56bf9a
Feng Feng <F.Feng@outlook.com>
2017-08-12 20:03:44 +0800

    Remove an un-used argument in FactorWithSign

--- Apart.m
+++ Apart.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
-(* ::Subsection:: *)
+(* ::Subsection::Closed:: *)
 (*InnerApart*)
 
 
@@ -39,6 +39,7 @@ Clear[InnerApart,CachedApart];
 ClearCachedApart[]:=Clear[CachedApart];
 
 
+ApartNull::usage="ApartNull=1 @ Integrand; ApartNull=0 @ Loop Integral";
 InnerApart[pcs_List,cs_List,np:{0...},vars_List]:=ApartNull;
 InnerApart[pcs_List,cs_List,np:{___,0,___},vars_List]:=InnerApart[Sequence@@Transpose@DeleteCases[{pcs,cs,np}//Transpose,p_List/;Part[p,3]===0],vars];
 
@@ -117,7 +118,7 @@ ClearAll[FactorWithSign,ApartVars];
 SignVars::usage="SignVars->{vars} is an Options of ApartVars";
 VarsSign::usage="VarsSign->1 or -1 is an Options of ApartVars";
 Options[ApartVars]={SignVars->{},VarsSign->-1};
-FactorWithSign[exp_,pnSign_,OptionsPattern[ApartVars]]:=Block[{tmp,pi,sign},
+FactorWithSign[exp_,OptionsPattern[ApartVars]]:=Block[{tmp,pi,sign},
 tmp=Map[Coefficient[exp,#]&,OptionValue[SignVars]];
 If[Length[DeleteCases[tmp,0]]==0,Return[1]];
 pi=First[FirstPosition[tmp,Except[0],Heads->False]];
@@ -136,7 +137,7 @@ tmp=InnerLog[Factor[exp]];
 logs=Union[Cases[tmp,_InnerLog,{0,Infinity}]]/.InnerLog->Identity;
 Scan[Function[x,If[Not[PolynomialQ[x,vars]||FreeQ[x,Alternatives@@vars]],Print["Error: ",x," is not Polynomial of ",vars];Abort[]]],logs];
 Scan[Function[x,If[Length[Normal[CoefficientArrays[x,vars]]]>2,Print["Error: ",x," is not Linear Polynomial of ",vars];Abort[]]],logs];
-tmp=Distribute[VF[tmp]]/.VF[c_. InnerLog[y_]]:>c (VF[y/FactorWithSign[y,Sign[c]]//Factor]+InnerLog[FactorWithSign[y,Sign[c]]]);
+tmp=Distribute[VF[tmp]]/.VF[c_. InnerLog[y_]]:>c (VF[y/FactorWithSign[y]//Factor]+InnerLog[FactorWithSign[y]]);
 On[Assert];Assert[FreeQ[tmp,_FactorWithSign]];
 tmp=tmp/.InnerLog[1]->0/.VF[x_]:>InnerLog[Hold[x]];
 tmp=Collect[tmp,_InnerLog];
